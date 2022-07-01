8a59e185ba95933592f387887ee25b92a7b01394
rod3539 <salix0228@gmail.com>
2022-07-01 19:48:52 +0900

    수정

--- 자료처리/후위표기법1.py
+++ 자료처리/후위표기법1.py
@@ -1,28 +1,20 @@
 n = int(input())
 num_list = list(input().split())
 stack = []
+opp = ['+', '-', '*', '/']
 
 for num in num_list:
-    if num == '+':
-        A = stack.pop()
-        B = stack.pop()
-        num = A + B
-        stack.append(num)
-    elif num == '-':
-        A = stack.pop()
-        B = stack.pop()
-        num = A - B
-        stack.append(num)
-    elif num == '*':
-        A = stack.pop()
-        B = stack.pop()
-        num = A * B
-        stack.append(num)
-    elif num == '/':
-        A = stack.pop()
-        B = stack.pop()
-        num = B // A
-        stack.append(num)
+    if num in opp:
+        if num == '+':
+            stack.append(stack.pop() + stack.pop())
+        elif num == '-':
+            A, B = stack.pop(), stack.pop()
+            stack.append(B - A)
+        elif num == '*':
+            stack.append(stack.pop() * stack.pop())
+        elif num == '/':
+            A, B = stack.pop(), stack.pop()
+            stack.append(B // A)
     else:
         stack.append(int(num))
 print(int(stack[0]))
