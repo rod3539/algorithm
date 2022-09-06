ce7487b3bd6bab89381a221110cf96cab851e6fa
rod3539 <salix0228@gmail.com>
2022-09-06 22:25:12 +0900

    dfs기초

---
+++ dfs/순열 사이클.py
@@ -0,0 +1,18 @@
+def dfs(v):
+    visited[v] = 1
+    number = numbers[v]
+    if not visited[number]:
+        dfs(number)
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    numbers = [0] + list(map(int, input().split()))
+    visited = [1] + [0] * N
+    result = 0
+    for i in range(1, N + 1):
+        if not visited[i]:
+            dfs(i)
+            result += 1
+    print(result)
