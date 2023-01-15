6f8920c08584aa502f9977f3df6ab74d804a892c
rod3539 <salix0228@gmail.com>
2023-01-15 21:02:51 +0900

    최솟값 만들기

--- sw아카데미 problem solving/D3/2806.n-queen.py
+++ sw아카데미 problem solving/D3/2806.n-queen.py
@@ -1,21 +1,23 @@
 def nQueen(k):
     if k == N:
-        global ans; ans += 1
+        global answer
+        answer += 1
     else:
         for i in range(N):
-            a, b = k + i, k - i + N -1
-            if col[i] or line1[a] or line2[b]: continue
+            a, b = k + i, k - i + N - 1
+            if col[i] or line1[a] or line2[b]:
+                continue
             col[i] = line1[a] = line2[b] = 1
             nQueen(k + 1)
             col[i] = line1[a] = line2[b] = 0
 
 
-
-for tc in range(1, int(input())+1):
+T = int(input())
+for tc in range(1, T+1):
     N = int(input())
     col = [0] * N
     line1 = [0] * (N * 2 - 1)
     line2 = [0] * (N * 2 - 1)
-    ans = 0
+    answer = 0
     nQueen(0)
-    print('#{} {}'.format(tc, ans))
+    print('#{} {}'.format(tc, answer))
