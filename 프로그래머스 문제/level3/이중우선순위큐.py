26dd168d50dd66a119804f5b9d67357301c55d5d
rod3539 <salix0228@gmail.com>
2023-01-23 23:01:35 +0900

    이중우선순위큐

--- 프로그래머스 문제/level3/이중우선순위큐.py
+++ 프로그래머스 문제/level3/이중우선순위큐.py
@@ -1,3 +1,25 @@
+import heapq
+
+
 def solution(operations):
-    answer = []
-    return answer
+    min_heap = []
+    for value in operations:
+        order, data = value.split()
+        data = int(data)
+        # 최댓값 삭제
+        if order == "D" and data == 1:
+            if min_heap:
+                max_value = max(min_heap)
+                min_heap.remove(max_value)
+        # 최솟값 삭제
+        elif order == "D" and data == -1:
+            if min_heap:
+                heapq.heappop(min_heap)
+        # 큐 데이터 삽입
+        else:
+            heapq.heappush(min_heap, data)
+
+    # heap이 비어있는 경우
+    if not min_heap:
+        return [0, 0]
+    return max(min_heap), min_heap[0]
