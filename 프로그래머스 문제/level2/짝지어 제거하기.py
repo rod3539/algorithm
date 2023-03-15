190e3f38310ac717d8df7f8505b983b99189f606
rod3539 <salix0228@gmail.com>
2023-03-15 21:04:18 +0900

    구명보트

--- 프로그래머스 문제/level2/짝지어 제거하기.py
+++ 프로그래머스 문제/level2/짝지어 제거하기.py
@@ -1,15 +1,3 @@
 def solution(s):
-    stack = []
-    for i in range(len(s)):
-        if not stack:
-            stack.append(s[i])
-        else:
-            if stack[-1] == s[i]:
-                stack.pop()
-            else:
-                stack.append(s[i])
-
-    if not stack:
-        return 1
-    else:
-        return 0
+    
+    return
--- 프로그래머스 문제/main.py
+++ 프로그래머스 문제/main.py
@@ -1,17 +1,32 @@
-import heapq
-def solution(scoville, K):
-    if min(scoville) >= K:
-        return 0
-    answer = 0
-    heapq.heapify(scoville)
-    while True:
-        a = heapq.heappop(scoville)
-        if a >= K:
-            break
-        b = heapq.heappop(scoville)
-        c = a + b * 2
-        heapq.heappush(scoville, c)
-        answer += 1
-        if len(scoville) == 1 and scoville[0] < K:
-            return -1
+from heapq import heappush, heappop
+def solution(n, s, a, b, fares):
+    answer = 1e9
+
+    def djikstra(start):
+        distance = [1e9] * (n + 1)
+        queue = []
+        heappush(queue, (0, start))
+        distance[start] = 0
+        while queue:
+            dist, now = heappop(queue)
+            if distance[now] < dist:
+                continue
+            for i in graph[now]:
+                cost = dist + i[1]
+                if cost < distance[i[0]]:
+                    distance[i[0]] = cost
+                    heappush(queue, (cost, i[0]))
+
+        return distance
+
+    graph = [[] for _ in range(n+1)]
+    for c, d, e in fares:
+        graph[c].append((d, e))
+        graph[d].append((c, e))
+    arr = [[]]
+    for i in range(1, n + 1):
+        arr.append(djikstra(i))
+    for i in range(1, n+1):
+        answer = min(answer, arr[s][i] + arr[i][a] + arr[i][b])
+
     return answer
