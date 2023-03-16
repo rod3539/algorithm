eedf9f845cce2f8c3980086f30a2ee4cbc4047c5
rod3539 <salix0228@gmail.com>
2023-03-16 21:46:06 +0900

    야근지수

--- 프로그래머스 문제/level2/짝지어 제거하기.py
+++ 프로그래머스 문제/level2/짝지어 제거하기.py
@@ -1,3 +1,18 @@
 def solution(s):
-    
-    return
+    stack = []
+
+    for w in s:
+        if not stack:
+            stack.append(w)
+        else:
+            if stack[-1] == w:
+                stack.pop()
+            else:
+                stack.append(w)
+    if not stack:
+        return 1
+    else:
+        return 0
+
+print(solution('baabaa'))
+print(solution('cdcd'))
